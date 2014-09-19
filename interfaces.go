24403a53889252f7ee47694bb59398e420c12165
Jon Wear <jonwear@fastmail.fm>
2014-09-18 22:23:16 -0400

    Forgot to save

--- interfaces.go
+++ interfaces.go
@@ -1,8 +1,1 @@
 package main
-
-type IOperator interface {
-	Add() (float64, string, error)
-	Subtract() (float64, string, error)
-	Multiply() (float64, string, error)
-	Divide() (float64, string, error)
-}
