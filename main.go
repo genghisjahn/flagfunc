9112ebcd7a22ba5cb9108ef1899a259ff3a2fe14
Jon Wear <jonwear@fastmail.fm>
2014-09-18 22:19:12 -0400

    Doesn't work, but we're getting there

--- main.go
+++ main.go
@@ -1,69 +1,26 @@
 package main
 
 import (
-	"errors"
-	"flag"
+	"fmt"
 	"log"
 )
 
-var (
-	operation = flag.String("op", "none", "Operation to perform on numbers 1 & 2.")
-	num1      = flag.Float64("num1", 0.0, "First Number (float64)")
-	num2      = flag.Float64("num2", 0.0, "Second Number (float64)")
-
-	operations = map[string]fnOperation{
-		"none": NoneOperator,
-		"add":  AddOperator,
-		"sub":  SubOperator,
-		"mul":  MultiOperator,
-		"div":  DivOperator,
-	}
-)
-
-type fnOperation func(num1 float64, num2 float64) (float64, string, error)
-
 func main() {
-	flag.Parse()
+
 	log.Println("FlagFunc - How to use flags with functions and interfaces.")
-	if operations[*operation] == nil {
-		log.Printf("Valid Values are:\n")
-		for key, _ := range operations {
-			log.Println(key)
-		}
-		log.Fatalf("Operation %v does not exist.", *operation)
+
+	normalOp := NormalOperator{}
+	normalOp.Num1 = 5.0
+	normalOp.Num2 = 5.0
+
+	ShowAddition(normalOp)
+
+}
+
+func ShowAddition(op IOperator) {
+	if result, problem, err := op.Add(); err != nil {
+		log.Println("Error Addition: ", err)
 	} else {
-		fnOperator := operations[*operation]
-		if result, sign, err := fnOperator(*num1, *num2); err != nil {
-			log.Println("Error:", err)
-			if sign == "none" {
-				log.Println("Try these parameters: -num1 5 -num2 10 -op add")
-			}
-		} else {
-			log.Printf("%v %v %v = %v", *num1, sign, *num2, result)
-		}
+		fmt.Println(problem, result)
 	}
-
-}
-
-func NoneOperator(num1 float64, num2 float64) (float64, string, error) {
-	return 0.0, "none", errors.New("No operation was specified!")
-}
-
-func AddOperator(num1 float64, num2 float64) (float64, string, error) {
-	return float64(num1 + num2), "+", nil
-}
-
-func SubOperator(num1 float64, num2 float64) (float64, string, error) {
-	return float64(num1 - num2), "-", nil
-}
-
-func MultiOperator(num1 float64, num2 float64) (float64, string, error) {
-	return float64(num1 * num2), "*", nil
-}
-
-func DivOperator(num1 float64, num2 float64) (float64, string, error) {
-	if num2 == 0 {
-		return 0.0, "", errors.New("For division (div), num2 cannot equal 0.")
-	}
-	return float64(num1) / float64(num2), "/", nil
 }
--- operators.go
+++ operators.go
@@ -1,6 +1,9 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 type NormalOperator struct {
 	Num1 float64
@@ -8,20 +11,20 @@ type NormalOperator struct {
 }
 
 func (op *NormalOperator) Add() (float64, string, error) {
-	return op.Num1 + op.Num2, "+", nil
+	return op.Num1 + op.Num2, fmt.Sprintf("%v + %v = ", op.Num1, op.Num2), nil
 }
 
 func (op *NormalOperator) Subtract() (float64, string, error) {
-	return op.Num1 - op.Num2, "-", nil
+	return op.Num1 - op.Num2, fmt.Sprintf("%v - %v = ", op.Num1, op.Num2), nil
 }
 
 func (op *NormalOperator) Multiply() (float64, string, error) {
-	return op.Num1 * op.Num2, "*", nil
+	return op.Num1 * op.Num2, fmt.Sprintf("%v * %v = ", op.Num1, op.Num2), nil
 }
 
 func (op *NormalOperator) Divide() (float64, string, error) {
 	if op.Num2 == 0.0 {
 		return 0.0, "", errors.New("For division (div), Num2 cannot equal 0.")
 	}
-	return op.Num1 / op.Num2, "/", nil
+	return op.Num1 / op.Num2, fmt.Sprintf("%v / %v = ", op.Num1, op.Num2), nil
 }
