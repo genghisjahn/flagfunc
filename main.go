8f4428703274c74c77f97f22e30fc65860c32dcf
Jon Wear <jonwear@fastmail.fm>
2014-09-18 22:22:47 -0400

    Not sure why all this has to be in the same file

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 )
@@ -13,7 +14,7 @@ func main() {
 	normalOp.Num1 = 5.0
 	normalOp.Num2 = 5.0
 
-	ShowAddition(normalOp)
+	ShowAddition(&normalOp)
 
 }
 
@@ -23,4 +24,35 @@ func ShowAddition(op IOperator) {
 	} else {
 		fmt.Println(problem, result)
 	}
+}
+
+type NormalOperator struct {
+	Num1 float64
+	Num2 float64
+}
+
+func (op *NormalOperator) Add() (float64, string, error) {
+	return op.Num1 + op.Num2, fmt.Sprintf("%v + %v = ", op.Num1, op.Num2), nil
+}
+
+func (op *NormalOperator) Subtract() (float64, string, error) {
+	return op.Num1 - op.Num2, fmt.Sprintf("%v - %v = ", op.Num1, op.Num2), nil
+}
+
+func (op *NormalOperator) Multiply() (float64, string, error) {
+	return op.Num1 * op.Num2, fmt.Sprintf("%v * %v = ", op.Num1, op.Num2), nil
+}
+
+func (op *NormalOperator) Divide() (float64, string, error) {
+	if op.Num2 == 0.0 {
+		return 0.0, "", errors.New("For division (div), Num2 cannot equal 0.")
+	}
+	return op.Num1 / op.Num2, fmt.Sprintf("%v / %v = ", op.Num1, op.Num2), nil
+}
+
+type IOperator interface {
+	Add() (float64, string, error)
+	Subtract() (float64, string, error)
+	Multiply() (float64, string, error)
+	Divide() (float64, string, error)
 }
